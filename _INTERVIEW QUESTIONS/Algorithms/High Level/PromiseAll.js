905d9c282b692f55cb0eea3624b6faa568178f4b
Ismoil Shokirov <ismoil.793@gmail.com>
2022-08-15 10:08:01 +0500

    updates

---
+++ _INTERVIEW QUESTIONS/Algorithms/High Level/PromiseAll.js
@@ -0,0 +1,30 @@
+function promiseAll(promises) {
+    const output = []
+    let counter = 0
+
+    return new Promise((resolve, reject) => {
+        promises.forEach((promise, i) => {
+            promise.then(result => {
+                output[i] = result
+                counter++
+                if (counter === promises.length) {
+                    resolve(output)
+                }
+            }).catch(reject)
+        })
+    })
+}
+
+const slowPromise = new Promise((res) => {
+    setTimeout(() => {
+        res('slow')
+    }, 100)
+})
+
+const promises = [
+    slowPromise,
+    Promise.resolve('second'),
+    Promise.resolve('last'),
+]
+
+promiseAll(promises).then(console.log)
--- _INTERVIEW QUESTIONS/Algorithms/travelingRobot.js
+++ _INTERVIEW QUESTIONS/Algorithms/travelingRobot.js
@@ -1,13 +1,30 @@
 // our robot is located on x=1, y=1
 
 function paths(x, y) { // x and y are coordinates of exit
-    if(x < 1 || y < 1) {
+    if (x < 1 || y < 1) {
         return 0; // if exit is on left side or bottom of robot - There is no way for exit 
     }
-    if(x === 1 && y === 1) {
+    if (x === 1 && y === 1) {
         return 1; // Robot and Exit are on same place
     }
-    return paths(x-1, y) + paths(x, y-1);
+    return paths(x - 1, y) + paths(x, y - 1);
 }
-
 // ------------------------- Big O(2^(x+y))
+
+console.log(paths(5, 4))
+
+
+
+function numberOfPaths(m, n) {
+    // We have to calculate m+n-2 C n-1 here
+    // which will be (m+n-2)! / (n-1)! (m-1)!
+    var path = 1;
+    for (i = n; i < (m + n - 1); i++) {
+        path *= i;
+        path = parseInt(path / (i - n + 1));
+    }
+    return path;
+}
+// ------------------------- Big O(m+n)
+
+console.log(numberOfPaths(5, 4))
